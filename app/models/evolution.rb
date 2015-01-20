17743a35395f810becb63395d94d7eb640703ee1
jjccc <jjcanocortes@outlook.com>
2015-01-20 01:08:40 +0100

    close #30

--- app/models/evolution.rb
+++ app/models/evolution.rb
@@ -1,18 +1,30 @@
 class Evolution
 
-  attr_accessor :data
+  attr_accessor :daily_ocurrences, :accumulated_ocurrences, :followers, :days
+  
   
   def initialize(range)
-    self.data = {}
+    self.days = []
+    self.daily_ocurrences = {}
+    self.accumulated_ocurrences = {}
+    self.followers = {}
     
     zone = ActiveSupport::TimeZone.new("Madrid")
     origin_date = Time.now.in_time_zone(zone).beginning_of_day - (range - 1).days
     counts = User.where("created_at >= ?", origin_date).group("date_trunc('day', created_at)").count
-     
+    
+    accumulated = 0
+    
     range.times do |i|
       date_key = (origin_date + i.days).strftime("%Y-%m-%d 00:00:00")
       ocurrences = counts[date_key] || 0
-      self.data[(origin_date + i.days).strftime("%d/%m")] = ocurrences
+      self.days << (origin_date + i.days).strftime("%d/%m")
+      self.daily_ocurrences[(origin_date + i.days).strftime("%d/%m")] = ocurrences
+      self.accumulated_ocurrences[(origin_date + i.days).strftime("%d/%m")] = ocurrences + accumulated
+      self.followers[(origin_date + i.days).strftime("%d/%m")] = User.where("created_at >= ?", origin_date).
+                                                                      where("created_at <= ?", origin_date + i.days).
+                                                                      sum(:followers)
+      accumulated += ocurrences
     end
   end
     
